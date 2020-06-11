49784f7313ba858182495cb4b9592df0b1394130
Masahiro Sakai <masahiro.sakai@gmail.com>
2020-06-11 12:09:41 +0900

    rewrite generate_maxsat_instances.py to use argparse

--- generate_maxsat_instances.py
+++ generate_maxsat_instances.py
@@ -1,7 +1,8 @@
+import argparse
 import copy
 import math
 from pathlib import Path
-from typing import Counter, Dict, List, Optional, Sequence, Tuple, Union
+from typing import Counter, Dict, List, Optional, Sequence, Tuple
 
 import chainer
 import numpy as np
@@ -12,36 +13,15 @@ import datasets
 import encoder
 
 
-result_dir = Path("instances/maxsat")
-
-debug = False
-
-debug_truelabel = False
-
-debug_sat = False
-
-produce_wbo = True
-
-produce_wcnf = True
-
-instances_per_class = 10
-
-problems = [
-    ("mnist", "models/mnist.npz"),
-    ("mnist_back_image", "models/mnist_back_image.npz"),
-    ("mnist_rot", "models/mnist_rot.npz"),
-]
-
-
-def add_norm(enc: encoder.Encoder, norm: Union[int, str],
+def add_norm(enc: encoder.Encoder, norm: str,
              mod: Sequence[Tuple[encoder.Lit, Optional[int]]]) -> None:
-    if norm == 0:
+    if norm == '0':
         for lit, w in mod:
             enc.add_clause([lit], None if w is None else 1)
-    elif norm == 1:
+    elif norm == '1':
         for lit, w in mod:
             enc.add_clause([lit], None if w is None else abs(w))
-    elif norm == 2:
+    elif norm == '2':
         for lit, w in mod:
             enc.add_clause([lit], None if w is None else w*w)
     elif norm == 'inf':
@@ -69,122 +49,135 @@ def add_norm(enc: encoder.Encoder, norm: Union[int, str],
         raise RuntimeError("unknown norm: " + str(norm))
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, choices=['mnist', 'mnist_back_image', 'mnist_rot'], default='mnist', help='dataset name')
+parser.add_argument('--model', type=str, default=None, help='model file (*.npz)')
+parser.add_argument('-o', '--output-dir', type=str, default="instances/maxsat", help='output directory')
+parser.add_argument('--format', type=str, choices=["wbo", "wcnf"], help='file format')
+parser.add_argument('--norm', type=str, choices=['0', '1', '2', 'inf'], default='inf', help='encoding of cardinality constraints')
+parser.add_argument('--card', type=str, choices=["sequential", "parallel"], default="parallel", help='encoding of cardinality constraints')
+parser.add_argument('--target', type=str, default="adversarial", choices=['adversarial', 'truelabel'], help='target label')
+parser.add_argument('--instance-no', type=int, default=None, help='specify instance number')
+parser.add_argument('--instances-per-class', type=int, default=None, help='number of instances to generate per class')
+parser.add_argument('--debug-sat', action='store_true', help='produce CNF or OPB for debug')
+parser.add_argument('--restrict', type=float, default=None, help='restrict search space to most salient pixels')
+
+
+args = parser.parse_args()
+
+result_dir = Path(args.output_dir)
 result_dir.mkdir(parents=True, exist_ok=True)
 
-encodings = \
-    ([("wbo", "")] if produce_wbo else []) + \
-    ([("wcnf", "parallel")] if produce_wcnf else [])
-# , ("wcnf", "sequential")
+train, test = datasets.get_dataset(args.dataset)
 
+if args.model is None:
+    weights_filename = f"models/{args.dataset}.npz"
+else:
+    weights_filename = args.model
+neurons = [28 * 28, 200, 100, 100, 100, 10]
+model = bnn.BNN(neurons, stochastic_activation=True)
+chainer.serializers.load_npz(weights_filename, model)
 
-for dataset_name, weights_filename in problems:
-    train, test = datasets.get_dataset(dataset_name)
+orig_image_scaled = test._datasets[0]
+orig_image_scaled = orig_image_scaled[:1000]
+orig_image = np.round(orig_image_scaled * 255).astype(np.uint8)
+with chainer.using_config("train", False), chainer.using_config("enable_backprop", False):
+    orig_image_bin = bnn.bin(model.input_bn(orig_image_scaled)).array > 0
+    orig_logits = model(orig_image_scaled).array
+predicated_label = np.argmax(orig_logits, axis=1)
 
-    neurons = [28 * 28, 200, 100, 100, 100, 10]
-    model = bnn.BNN(neurons, stochastic_activation=True)
-    chainer.serializers.load_npz(weights_filename, model)
+if args.format == "wbo":
+    enc_base = encoder.BNNEncoder(cnf=False)
+elif args.format == "wcnf":
+    enc_base = encoder.BNNEncoder(cnf=True, counter=args.card)
+else:
+    raise RuntimeError("unknown ext: " + args.format)
+inputs = enc_base.new_vars(784)
+outputs = enc_base.new_vars(10)
+enc_base.encode_bin_input(model, inputs, outputs)
 
-    orig_image_scaled = test._datasets[0]
-    orig_image = np.round(orig_image_scaled * 255).astype(np.uint8)
-    with chainer.using_config("train", False), chainer.using_config("enable_backprop", False):
-        orig_image_bin = bnn.bin(model.input_bn(orig_image_scaled)).array > 0
-        orig_logits = model(orig_image_scaled).array
-    predicated_label = np.argmax(orig_logits, axis=1)
+counter = Counter[int]()
 
-    for ext, counter_encoding in encodings:
-        if ext == "wbo":
-            enc_base = encoder.BNNEncoder(cnf=False)
-        elif ext == "wcnf":
-            enc_base = encoder.BNNEncoder(cnf=True, counter=counter_encoding)
+for instance_no, (x, true_label) in enumerate(test):
+    if args.instance_no is not None and instance_no != args.instance_no:
+        continue
+    if args.instances_per_class is not None and counter[true_label] >= args.instances_per_class:
+        continue
+    print(f"dataset={args.dataset}; instance={instance_no}; true_label={true_label} predicted_label={predicated_label[instance_no]}")
+    if predicated_label[instance_no] != true_label:
+        continue
+    counter[true_label] += 1
+
+    img = PIL.Image.fromarray(orig_image[instance_no].reshape(28, 28))
+    fname = result_dir / f"bnn_{args.dataset}_{instance_no}_label{true_label}.png"
+    if not fname.exists():
+        img.save(fname)
+
+    with chainer.using_config("train", False), chainer.using_config("enable_backprop", True):
+        saliency_map = model.saliency_map(x, true_label)
+
+    enc = copy.copy(enc_base)
+    if args.target == "truelabel":
+        enc.add_clause([outputs[true_label]])
+    else:
+        enc.add_clause([-outputs[true_label]])
+
+    input_bn = model.input_bn
+    mu = input_bn.avg_mean
+    sigma = np.sqrt(input_bn.avg_var + input_bn.eps)
+    gamma = input_bn.gamma.array
+    beta = input_bn.beta.array
+
+    mod: List[Tuple[encoder.Lit, Optional[int]]] = []
+    for j, pixel in enumerate(orig_image[instance_no]):
+        C_frac = 255 * (- beta[j] * sigma[j] / gamma[j] + mu[j])
+        if gamma[j] >= 0:
+            # x ≥ ⌈255 (- βσ/γ + μ)⌉ = C
+            C = int(math.ceil(C_frac))
+            assert orig_image_bin[instance_no, j] == (pixel >= C)
+            if pixel < C:
+                mod.append((- inputs[j], C - pixel))
+            elif C == 0:
+                mod.append((inputs[j], None))  # impossible to change
+            else:
+                mod.append((inputs[j], (C - 1) - pixel))
         else:
-            raise RuntimeError("unknown ext: " + ext)
-        inputs = enc_base.new_vars(784)
-        outputs = enc_base.new_vars(10)
-        enc_base.encode_bin_input(model, inputs, outputs)
+            # x ≤ ⌊255 (- βσ/γ + μ)⌋ = C
+            C = int(math.floor(C_frac))
+            assert orig_image_bin[instance_no, j] == (pixel <= C)
+            if pixel > C:
+                mod.append((- inputs[j], C - pixel))
+            elif C == 255:
+                mod.append((inputs[j], None))  # impossible to change
+            else:
+                mod.append((inputs[j], (C + 1) - pixel))
 
-        counter = Counter[int]()
+    # debug
+    if args.debug_sat:
+        enc2 = copy.copy(enc)
+        for lit, w in mod:
+            enc2.add_clause([lit])
+        if args.format == "wcnf":
+            fname = result_dir / f"bnn_{args.dataset}_{instance_no}_label{true_label}_{args.target}_{args.card}_debug.cnf"
+        elif args.format == "wbo":
+            fname = result_dir / f"bnn_{args.dataset}_{instance_no}_label{true_label}_{args.target}_debug.opb"
+        else:
+            raise RuntimeError("unknown ext: " + args.format)
+        enc2.write_to_file(fname)
 
-        for instance_no, (x, true_label) in enumerate(test):
-            if counter[true_label] >= instances_per_class:
-                continue
-            print(f"dataset={dataset_name}; instance={instance_no}; true_label={true_label} predicted_label={predicated_label[instance_no]}")
-            if predicated_label[instance_no] != true_label:
-                continue
-            counter[true_label] += 1
+    if args.restrict is None:
+        mod2 = mod
+        ratio = 1.0
+        ratio_str = ""
+    else:
+        ratio = args.restrict
+        ratio_str = f"{int(args.restrict * 100)}p"
+        important_pixels = set(list(reversed(np.argsort(np.abs(saliency_map))))[:int(len(saliency_map) * ratio)])
+        important_variables = set(inputs[instance_no] for i in important_pixels)
+        mod2 = [(lit, w if abs(lit) in important_variables else None) for lit, w in mod]
 
-            img = PIL.Image.fromarray(orig_image[instance_no].reshape(28, 28))
-            fname = result_dir / f"bnn_{dataset_name}_{instance_no}_label{true_label}.png"
-            if not fname.exists():
-                img.save(fname)
-
-            with chainer.using_config("train", False), chainer.using_config("enable_backprop", True):
-                saliency_map = model.saliency_map(x, true_label)
-
-            for target in ["adversarial"] + (["truelabel"] if debug_truelabel else []):
-                enc = copy.copy(enc_base)
-                if target == "truelabel":
-                    enc.add_clause([outputs[true_label]])
-                else:
-                    enc.add_clause([-outputs[true_label]])
-
-                input_bn = model.input_bn
-                mu = input_bn.avg_mean
-                sigma = np.sqrt(input_bn.avg_var + input_bn.eps)
-                gamma = input_bn.gamma.array
-                beta = input_bn.beta.array
-
-                mod: List[Tuple[encoder.Lit, Optional[int]]] = []
-                for j, pixel in enumerate(orig_image[instance_no]):
-                    C_frac = 255 * (- beta[j] * sigma[j] / gamma[j] + mu[j])
-                    if gamma[j] >= 0:
-                        # x ≥ ⌈255 (- βσ/γ + μ)⌉ = C
-                        C = int(math.ceil(C_frac))
-                        assert orig_image_bin[instance_no, j] == (pixel >= C)
-                        if pixel < C:
-                            mod.append((- inputs[j], C - pixel))
-                        elif C == 0:
-                            mod.append((inputs[j], None))  # impossible to change
-                        else:
-                            mod.append((inputs[j], (C - 1) - pixel))
-                    else:
-                        # x ≤ ⌊255 (- βσ/γ + μ)⌋ = C
-                        C = int(math.floor(C_frac))
-                        assert orig_image_bin[instance_no, j] == (pixel <= C)
-                        if pixel > C:
-                            mod.append((- inputs[j], C - pixel))
-                        elif C == 255:
-                            mod.append((inputs[j], None))  # impossible to change
-                        else:
-                            mod.append((inputs[j], (C + 1) - pixel))
-
-                # debug
-                if debug_sat:
-                    enc2 = copy.copy(enc)
-                    for lit, w in mod:
-                        enc2.add_clause([lit])
-                    if ext == "wcnf":
-                        fname = result_dir / f"bnn_{dataset_name}_{instance_no}_label{true_label}_{target}_{counter_encoding}_debug.cnf"
-                    elif ext == "wbo":
-                        fname = result_dir / f"bnn_{dataset_name}_{instance_no}_label{true_label}_{target}_debug.opb"
-                    else:
-                        raise RuntimeError("unknown ext: " + ext)
-                    enc2.write_to_file(fname)
-
-                for ratio, ratio_str in [(1.0, ""), (0.2, "20p")]:  # , (0.5, "50p")
-                    if ratio != 1.0:
-                        important_pixels = set(list(reversed(np.argsort(np.abs(saliency_map))))[:int(len(saliency_map) * ratio)])
-                        important_variables = set(inputs[instance_no] for i in important_pixels)
-                        mod2 = [(lit, w if abs(lit) in important_variables else None) for lit, w in mod]
-                    else:
-                        mod2 = mod
-
-                    norms: List[Union[int, str]] = [0, 1, 2, 'inf']
-                    for norm in norms:
-                        suffix = ''.join(["_" + s for s in (target, "norm_" + str(norm), ratio_str, counter_encoding) if len(s) > 0])
-                        fname = result_dir / f"bnn_{dataset_name}_{instance_no}_label{true_label}{suffix}.{ext}"
-                        enc2 = copy.copy(enc)
-                        add_norm(enc2, norm, mod2)
-                        enc2.write_to_file_opt(fname)
-
-            if debug:
-                break
+    suffix = ''.join(["_" + s for s in (args.target, "norm_" + str(args.norm), ratio_str, args.card) if len(s) > 0])
+    fname = result_dir / f"bnn_{args.dataset}_{instance_no}_label{true_label}{suffix}.{args.format}"
+    enc2 = copy.copy(enc)
+    add_norm(enc2, args.norm, mod2)
+    enc2.write_to_file_opt(fname)
